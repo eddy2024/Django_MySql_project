9a46ea16842e79266c4ed5c53c5d73c8eb7b8a8d
eddy2024 <eddysoftwarengineer@gmail.com>
2023-04-06 15:05:00 +0000

    created login and configuring authentication

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    # path('login/', views.login_user, name='login'),
+    # path('logout/', views.logout_user, name='logout'),
   
 ]
