77656ae0d83396ef79ee68235b5c482f6bde767a
eddy2024 <eddysoftwarengineer@gmail.com>
2023-04-12 11:02:59 +0000

    updated register form

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -32,7 +32,7 @@ def logout_user(request):
 
 # def register page
 def register_user(request):
-    if request.method == 'PSOT':
+    if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
@@ -41,9 +41,11 @@ def register_user(request):
             password = form.cleaned_data['password1']
             user = authenticate(username=username,  password=password)
             login(request, user)
-            messages.success(request, "You Have Successfully Registered Welcome")
+            messages.success(request, "You Have Successfully Registered!")
             return redirect('home')
         
     else:
         form = SignUpForm()    
         return render(request, 'register.html', {'form':form})
+    
+    return render(request, 'register.html', {'form':form})
