1c1b129926dc0000b8e6c9d38fd384f5cddf8ab9
eddy2024 <eddysoftwarengineer@gmail.com>
2023-04-12 08:50:31 +0000

    registration

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages 
+from django.contrib import messages     
+from .forms import SignUpForm
 
 
 # Create your views here.
@@ -28,3 +29,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You Have Been Logged Out.....!")
     return redirect('home')
+
+# def register page
+def register_user(request):
+    if request.method == 'PSOT':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username,  password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered Welcome")
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()    
+        return render(request, 'register.html', {'form':form})
